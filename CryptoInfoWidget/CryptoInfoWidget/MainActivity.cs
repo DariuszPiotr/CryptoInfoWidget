33f6d112be22cf0be6724ee9f2b6c03fa4b6e096
Darek <detror@gmail.com>
2017-10-15 20:30:29 +0200

    start
    new project

---
+++ CryptoInfoWidget/CryptoInfoWidget/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace CryptoInfoWidget
+{
+    [Activity(Label = "CryptoInfoWidget", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+           Button button = FindViewById<Button>(Resource.Id.button1);
+
+            button.Click += delegate { button.Text = "dasdasd"; };
+
+        }
+    }
+}
+
